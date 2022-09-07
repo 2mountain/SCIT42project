48ed70ad383acdc22ad00b12c4aed71c4c10bd56
San Lee <tks20616@naver.com>
2022-09-07 17:09:13 +0900

    security Changes

--- src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
+++ src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
@@ -7,11 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-public class HomeController {
-	
-	@GetMapping({"/", ""})
-	public String index() {
-		log.debug("test");
-		return "home";
+public class FacilitiesController {
+	@GetMapping("/facilities")
+	public String facilities() {
+		log.debug("w");
+		return "/facilities/facilities";
 	}
 }
