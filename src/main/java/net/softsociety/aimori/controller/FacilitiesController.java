e2d978d194eff98c027732002823304c00fec090
2tks <tks20616@gmail.com>
2022-09-07 23:00:30 +0900

    san

--- src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
+++ src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
@@ -2,13 +2,15 @@ package net.softsociety.aimori.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequestMapping("/facilities")
 @Controller
 public class FacilitiesController {
-	@GetMapping("/facilities")
+	@GetMapping("")
 	public String facilities() {
 		log.debug("[FacilitiesController] facilities.html");
 		return "/facilities/facilities";
