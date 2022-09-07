87dbf426be8ebe77fc5164a7afe9c4bfc15f54f2
2tks <tks20616@gmail.com>
2022-09-07 20:22:16 +0900

    facilities

--- src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
+++ src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 public class FacilitiesController {
 	@GetMapping("/facilities")
 	public String facilities() {
-		log.debug("w");
+		log.debug("[facilities]");
 		return "/facilities/facilities";
 	}
 }
--- src/main/java/net/softsociety/aimori/controller/HomeController.java
+++ src/main/java/net/softsociety/aimori/controller/HomeController.java
@@ -10,8 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 public class HomeController {
 	
 	@GetMapping({"/", ""})
-	public String index() {
-		log.debug("test");
+	public String home() {
+		log.debug("[home]");
 		return "home";
 	}
 }
