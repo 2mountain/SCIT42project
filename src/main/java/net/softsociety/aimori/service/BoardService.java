6864b62539c65cca840404f78717d4634eea4e15
lylue10 <ky_lue@naver.com>
2022-09-11 22:30:53 +0900

    09.11 boardlist

---
+++ src/main/java/net/softsociety/aimori/service/BoardService.java
@@ -0,0 +1,15 @@
+package net.softsociety.aimori.service;
+
+import java.util.ArrayList;
+
+import net.softsociety.aimori.domain.Board;
+import net.softsociety.aimori.util.PageNavigator;
+
+public interface BoardService {
+
+	//페이징 정보
+	public PageNavigator getPageNavigator(int pagePerGroup, int countPerPage, int page, String type, String searchWord);
+	//현재 페이지 글 목록
+	public ArrayList<Board> list(PageNavigator navi, String type, String searchWord);
+
+}
