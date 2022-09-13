951a2e46d0a5990618e96f66d4969da8b47823cc
han <dirkgos1994@naver.com>
2022-09-13 09:43:58 +0900

    s

---
+++ src/main/java/net/softsociety/aimori/service/MemberService.java
@@ -0,0 +1,11 @@
+package net.softsociety.aimori.service;
+
+import net.softsociety.aimori.domain.Member;
+
+public interface MemberService {
+	// 회원가입
+	public int signInMember(Member member);
+	
+	// ID 중복확인
+	public boolean idCheck(String cId);
+}
