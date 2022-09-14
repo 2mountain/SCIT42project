00d2c35eeb7680147abb9d2cbe10d8c8ea6033f1
2mountain <tks20616@naver.com>
2022-09-14 18:13:36 +0900

    facilities 80%

--- src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
+++ src/main/java/net/softsociety/aimori/controller/FacilitiesController.java
@@ -1,8 +1,11 @@
 package net.softsociety.aimori.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,8 +15,22 @@ import lombok.extern.slf4j.Slf4j;
 public class FacilitiesController {
 	
 	@GetMapping({"", "/"})
-	public String facilities() {
+	public String facilities(Model model) {
+		String place = "애완동물";
+		
+		model.addAttribute("place", place);
+		
 		log.debug("[FacilitiesController] facilities.html");
+		
+		return "/facilities/facilities";
+	}
+	
+	@PostMapping({"", "/"})
+	public String placeSave(Model model, @RequestParam(name="place", defaultValue="애완동물") String place) {
+		log.debug("[FacilitiesController] placeSave - parameter : {}", place);
+
+		model.addAttribute("place", place);
+		
 		return "/facilities/facilities";
 	}
 }
