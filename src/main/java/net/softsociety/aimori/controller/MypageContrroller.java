d577db6883a927a68d633083f78ac10d40ccff47
unknown <corosson@gmail.com>
2022-09-20 17:40:43 +0900

    mypage ing

--- src/main/java/net/softsociety/aimori/controller/MypageContrroller.java
+++ src/main/java/net/softsociety/aimori/controller/MypageContrroller.java
@@ -1,20 +1,37 @@
 package net.softsociety.aimori.controller;
 
+import java.lang.reflect.Member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import net.softsociety.aimori.service.MemberService;
 
 @Slf4j
 @RequestMapping("mypage")
 @Controller
 public class MypageContrroller {
+	
+	@Autowired
+	MemberService service;
 
+	/**
+	 * 마이페이지 메인 진입
+	 * @return
+	 */
 	@GetMapping("main")
-	public String mypageMain() {
-		log.debug("성공");
-		return "mypageView/mypageMain";
+	public String mypageMain(@AuthenticationPrincipal UserDetails user, Model model) {
+		log.debug("UserDetails 정보 : {}", user);
+		//Member member = service.getMemberInfo(user.getUsername());
+				//getMemberInfo(user.getUsername());
+		//model.addAttribute("member", member);
+		return "/memberView/mypageMain";
 	}
 	
 	
