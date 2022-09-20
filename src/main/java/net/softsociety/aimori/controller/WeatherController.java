14d3db9b06fc9e8c724ecbd12af24387baa3ad90
unknown <corosson@gmail.com>
2022-09-20 09:59:31 +0900

    wallet

--- src/main/java/net/softsociety/aimori/controller/WeatherController.java
+++ src/main/java/net/softsociety/aimori/controller/WeatherController.java
@@ -3,9 +3,7 @@ package net.softsociety.aimori.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
