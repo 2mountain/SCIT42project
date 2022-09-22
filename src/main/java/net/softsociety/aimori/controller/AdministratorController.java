a1f971d422221a008f8d7df51994096062c6a9cc
2mountain <tks20616@naver.com>
2022-09-22 09:04:36 +0900

    hi

--- src/main/java/net/softsociety/aimori/controller/AdministratorController.java
+++ src/main/java/net/softsociety/aimori/controller/AdministratorController.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class AdministratorController {
 	@GetMapping({"","/"})
 	public String Administrator() {
-		// return "administrator/administrator";
+		log.debug("[AdministratorController] administrator : Hello!");
 		return "administrator/administrator";
 	}
 	
