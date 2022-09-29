b467c4c2dca849f4a3f5ddbf66444330aaa98ad6
2mountain <tks20616@naver.com>
2022-09-29 09:29:38 +0900

    SAN

--- src/main/resources/static/js/chart-pie-demo.js
+++ src/main/resources/static/js/chart-pie-demo.js
@@ -2,6 +2,40 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+let resultLabel = [];
+let resultData = [];
+
+window.onload = function(){
+	pieChartData();
+}
+
+function pieChartData(){
+	$.ajax({
+			url : 'administrator/getDogType',
+			type : 'post',
+			dataType : 'json',
+			success : function(result){
+				// 오늘 날짜 - 7일 사이의 가입자 수 받아오기
+				for(let i = 0; i < result.length; i++){
+					resultLabel.push(result[i].joinDate);
+					resultData.push(result[i].memberJoinCount);
+				}
+				console.log(resultLabel);
+				console.log(resultData);
+				
+				/*
+				$(function(){
+				    $("#myBarChart").load("administrator/administrator.html");
+				});
+				*/
+			},
+			error : function(e){
+				alert('차트 데이터 로딩 실패');
+				console.log(JSON.stringify(e));
+			}
+		});
+}
+
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
